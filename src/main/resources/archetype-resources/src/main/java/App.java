f794d68e3800e748d58e1fd44340398556764e41
Anuvarshini KS <anuvarshini.ad0408@gmail.com>
2026-03-25 15:22:36 +0530

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.kovanlabs.intern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
